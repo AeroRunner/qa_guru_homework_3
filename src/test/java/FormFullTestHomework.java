2507eea1aec6fa1ff96ca418f1de7f4e40b7fc41
AeroRunner <airplay1x6@gmail.com>
2023-10-27 14:24:54 +0300

    add Form test 1

--- src/test/java/FormFullTestHomework.java
+++ src/test/java/FormFullTestHomework.java
@@ -1,8 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class FormFullTestHomework {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
     @Test
-    void Test1(){
+    void fillFormTest() {
+        open("/text-box");
+        $("#userName").setValue("Some name");
+        $("#userEmail").setValue("airplay.com");
+        $("#currentAddress").setValue("Some street ");
+        $("#permanentAddress").setValue("Another street 1");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Some name"));
+        $("#output #email").shouldHave(text("airplay.com"));
+        $("#output #currentAddress").shouldHave(text("Some street "));
+        $("#output #permanentAddress").shouldHave(text("Another street "));
+    }
 
     }
-}
+
+
+
