23a33aaf6dbfc94d3380a857a793bd80746202be
AeroRunner <airplay1x6@gmail.com>
2023-10-27 17:27:45 +0300

    Form Test DONE!

--- src/test/java/FormFullTestHomework.java
+++ src/test/java/FormFullTestHomework.java
@@ -17,7 +17,6 @@ public class FormFullTestHomework {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
     }
 
     @Test
@@ -25,37 +24,39 @@ public class FormFullTestHomework {
 
         // Заполнение нужных полей анкеты
 
-        open("/automation-practice-form"); // Открытие нужной формы на сайте
-        $("#firstName").setValue("Dmitrii"); // Имя
-        $("#lastName").setValue("Elizarov"); // Фамилия
-        $("#userEmail").setValue("airplay1x6@gmail.com"); // Почта
-        $("[for='gender-radio-1']").click(); // Выбор гендера
-        $("#userNumber").setValue("8888888888"); // Номер телефона
-        $("#dateOfBirthInput").click(); // Клик по блоку с выбором дня рождения
-        $(".react-datepicker__year-select").click();  // Выбор года в блоке
-        $(byValue("1999")).click();
+        open("/automation-practice-form");   // Открытие нужной формы на сайте
+        $("#firstName").setValue("Dmitrii");
+        $("#lastName").setValue("Elizarov");       // Фамилия
+        $("#userEmail").setValue("airplay1x6@gmail.com");    // Почта
+        $("[for='gender-radio-1']").click();    // Выбор гендера
+        $("#userNumber").setValue("8888888888");     // Номер телефона
+        $("#dateOfBirthInput").click();     // Клик по блоку с выбором дня рождения
+        $(".react-datepicker__year-select").click();    // Выбор выпадающего списка с годами
+        $(byValue("1999")).click();   // Выбор самого года
 
         //Вариант выбора месяца в списке, но есть проблема с нумерованием - список идет с 0 до 11, где 11 должно быть December.Но есть баг и выбирается November, про это было обсуждение в чате
         // $(".react-datepicker__month-select").click();
         // $(byValue("0")).click();
 
-        $(".react-datepicker__month-select").selectOption("December"); // Другой вариант выбора месяца через options
-        $(".react-datepicker__day--029").click(); // Выбор дня в блоке
+        $(".react-datepicker__month-select").selectOption("December");   // Другой вариант выбора месяца через options
+        $(".react-datepicker__day--029").click();   // Выбор дня в блоке
         // Выбор предметов
-        $("#subjectsInput").val("Computer Science").pressEnter(); //
-        $("#subjectsInput").val("English").pressEnter();
         $("#subjectsInput").val("Computer Science").pressEnter();
-        $("#subjectsInput").val("Machs").pressEnter();
+
+        //$("#closeLargeModal").click(); // Вылезает модальное окно само, это костыль - чтобы закрывать его
+
         // Выбор хобби
         $("[for='hobbies-checkbox-1']").click();
         $("[for='hobbies-checkbox-2']").click();
         $("[for='hobbies-checkbox-3']").click();
-        $("#uploadPicture").uploadFile(new File("src/test/picture/testimage.png"));
-        $("#currentAddress").setValue("Some address");
-        $("#state").scrollTo().click();
+
+        $("#uploadPicture").uploadFile(new File("src/test/picture/testimage.png")); // Импорт изображения
+        $("#currentAddress").setValue("SOME ADRESS");
+
         $("#react-select-3-input").val("Haryana").pressEnter();
-        $("#city").click();
+
         $("#react-select-4-input").val("Karnal").pressEnter().pressTab().pressEnter();
+
 
 
 
